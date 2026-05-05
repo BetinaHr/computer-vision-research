bc3d2b229ef5dfb2f2787089ec40e6b954f00fff
Bertina Hristova <148110314+BetinaHr@users.noreply.github.com>
2026-05-05 17:09:52 +0300

    Add GitHub Pages workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
+const repoName = "computer-vision-research";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
